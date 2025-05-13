55f8a42e167576ca6e9374a39e25fd6a35412ddb
LuisHenrique08 <162449796+LuisHenrique08@users.noreply.github.com>
2025-05-13 12:09:04 -0300

    Add files via upload

--- revisao.js
+++ revisao.js
@@ -37,6 +37,8 @@ console.log(filme)
 
 // 5) Utilização de if-else
 
+
+console.log("####### EXERCICIO 5 #######")
 if (filme.Nota<=4)
     console.log("O filme possui uma nota considerada baixa.")
 if (filme.Nota>4&&filme.Nota<7)
@@ -45,35 +47,32 @@ if(filme.Nota>7)
     console.log("O filme é muito bom!")
 
 // 6) Criação de uma função que analisa os dados da previsão do tempo
-const previsaoTempo = [
-    {
-        dia: "segunda",
-        temperaturaMin: 14,
-        temperaturaMax: 19,
-        chanceDeChuva: 10
-    },
-    {
-        dia: "terça",
-        temperaturaMin: 13,
-        temperaturaMax: 20,
-        chanceDeChuva: 10
-    },
-    {
-        dia: "quarta",
-        temperaturaMin: 15,
-        temperaturaMax: 22,
-        chanceDeChuva: 80
-    },
-    {
-        dia: "quinta",
-        temperaturaMin: 13,
-        temperaturaMax: 18,
-        chanceDeChuva: 60
-    },
-    {
-        dia: "sexta",
-        temperaturaMin: 12,
-        temperaturaMax: 16,
-        chanceDeChuva: 40
+
+
+    function verificaChuva(previsaoTempo) {
+          console.log("\nPrevisão do tempo para a semana:");
+   
+         for (const dia of previsaoTempo) {
+        if (dia.chanceDeChuva > 50) {                                // chanceDeChuva
+            console.log(`${dia.dia}: levar guarda-chuva`);          // dia.dia
+        } else {
+            console.log(`${dia.dia}: tempo agradável`);           // dia.dia
+        }
     }
-]
+}
+
+// Lista de previsão do tempo
+
+
+const previsaoTempo = [
+    { dia: "Segunda", temperaturaMin: 14, temperaturaMax: 19, chanceDeChuva: 10 },
+
+    { dia: "Terça", temperaturaMin: 13, temperaturaMax: 20, chanceDeChuva: 10 },
+
+    { dia: "Quarta", temperaturaMin: 15, temperaturaMax: 22, chanceDeChuva: 80 },
+
+    { dia: "Quinta", temperaturaMin: 13, temperaturaMax: 18, chanceDeChuva: 60 },
+
+    { dia: "Sexta", temperaturaMin: 12, temperaturaMax: 16, chanceDeChuva: 40 }];
+
+verificaChuva(previsaoTempo);
